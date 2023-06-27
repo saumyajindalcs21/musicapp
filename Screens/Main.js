dc357fde826818c186302d20369d7078e15b4ef4
saumyajindalcs21 <136785507+saumyajindalcs21@users.noreply.github.com>
2023-06-27 08:43:42 +0530

    updated

--- Screens/Main.js
+++ Screens/Main.js
@@ -1,175 +1,290 @@
 import React from "react";
-import { View,Text,StyleSheet,Dimensions,Image,ScrollView,TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, Image, ScrollView, TouchableOpacity } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Footer } from "../Components/Footer";
 
-const menu =require('../assets/icons/menu.png');
-const searchkro =require('../assets/icons/searchkro.png');
-const card1= require('../assets/images/music.jpg')
-export function Main({navigation}){
-    const deviceH = Dimensions.get("screen").height;
-    const deviceW = Dimensions.get("screen").width;
-    return(
-        <View style= {styles.container}>
-         <StatusBar style="light" />
-         <View style = {styles.subCont}>
-            {/* search header start */}
-            <View style = {styles.searchHeader}>
-            <View style= {[styles.headerItems,{width:'25%'}]}>
+const menu = require('../assets/icons/menu.png');
+const searchkro = require('../assets/icons/searchkro.png');
+const card1 = require('../assets/images/music.jpg');
+const play  = require('../assets/images/play.png');
+export function Main({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="light" />
+      <View style={styles.subCont}>
+        {/* search header start */}
+        <View style={styles.searchHeader}>
+          <View style={[styles.headerItems, { width: '13%' }]}>
             <TouchableOpacity>
-                   <Image source= {menu} styles={styles.imgIcons} />
-                   </TouchableOpacity>
-                </View> 
-                <View style= {[styles.headerItems, {width:'150%', gap:10}]}>
+              <Image source={menu} style={styles.imgIcons} />
+            </TouchableOpacity>
+          </View>
+          <View style={[styles.headerItems, { flexDirection:'row',alignItems:'center',width: '80%', flexDirection: 'row',marginRight:11 }]}>
+            <TouchableOpacity style={{ marginRight:10 }}>
+              <Image source={searchkro} style={styles.imgIcons} />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Text style={styles.searchtext}>Search</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        {/* search header end */}
+
+        <Text style={styles.mainHeading}> Trending Right Now</Text>
+        <View style={{ marginBottom: 10 }}></View>
+        {/* Trending cards start */}
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          <View style={styles.cardContainer}>
+            <View style={styles.card}>
+              <Image source={card1} style={styles.cardImg} />
+              <TouchableOpacity style={styles.dotIcon}>
+                <Text style={styles.dotFont}>...</Text>
+              </TouchableOpacity>
+              <View style={styles.playbox}>
                 <TouchableOpacity>
-                <Image source={searchkro} styles={styles.imgIcons} />
-                </TouchableOpacity>
-                <View>
-                <TouchableOpacity>
-                    <Text style ={styles.searchtext}>
-                        Search
-                    </Text> 
-                    </TouchableOpacity>
-                </View></View>
-                 </View>
-                 <Text style={styles.mainHeading}>
-                    Trending Right Now</Text>
-                   {/* heading text ends*/}
-                   {/*  Trending cards start*/}
-                    <View>
-                    <ScrollView>
-                        <View style={styles.card}>
-                            <Image source={card1} style={styles.cardImg} />
-                            <TouchableOpacity  style={styles.dotIcon}>
-                            <Text style={styles.dotFont}>...</Text>
-                            </TouchableOpacity>
-                            <View style={styles.playbox}>
-                            <TouchableOpacity>
-                                <View style= {styles.playboxItem}>
-                                    <View>
-                                        <Text style={styles.musicName}>Faded</Text>
-                                    </View>
-                                    <View>
-                                        <Text style={styles.musicDesc}>🎵  song by Alan Walker</Text>
-                                    </View>
-                             </View>
-                             </TouchableOpacity>
-                             </View>
-                            </View>        
-                    </ScrollView>
-                    </View>
-                    {/* trending cards end */}
-                 </View>
+                  <View style={styles.playboxItem}>
+
+                    <Text style={styles.musicName}>Faded</Text>
+                    <Text style={styles.musicDesc}>🎵 song by Alan Walker</Text>
                     
-                {/* Search header ends */}
-                {/* heading text */}
-               
-           
-    
-  {  /* <Text style= {{
-                fontSize: 24,
-                fontWeight : '900',
-            }}>Home Screen</Text>  */}
-            
-             <Footer navigation={navigation} />
-             </View>
-             
-             ) }
+                  </View>
+                
+                </TouchableOpacity>
+                
+              </View>
+            </View>
+
+            <View style={styles.card}>
+              <Image source={card1} style={styles.cardImg} />
+              <TouchableOpacity style={styles.dotIcon}>
+                <Text style={styles.dotFont}>...</Text>
+              </TouchableOpacity>
+              <View style={styles.playbox}>
+                <TouchableOpacity>
+                  <View style={styles.playboxItem}>
+                    <Text style={styles.musicName}>Faded</Text>
+                    <Text style={styles.musicDesc}>🎵 song by Alan Walker</Text>
+                    
+                  </View>
+                  
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View style={styles.card}>
+              <Image source={card1} style={styles.cardImg} />
+              <TouchableOpacity style={styles.dotIcon}>
+                <Text style={styles.dotFont}>...</Text>
+              </TouchableOpacity>
+              <View style={styles.playbox}>
+                <TouchableOpacity>
+                  <View style={styles.playboxItem}>
+                    <Text style={styles.musicName}>Faded</Text>
+                    <Text style={styles.musicDesc}>🎵 song by Alan Walker</Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View style={styles.card}>
+              <Image source={card1} style={styles.cardImg} />
+              <TouchableOpacity style={styles.dotIcon}>
+                <Text style={styles.dotFont}>...</Text>
+              </TouchableOpacity>
+              <View style={styles.playbox}>
+                <TouchableOpacity>
+                  <View style={styles.playboxItem}>
+                    <Text style={styles.musicName}>Faded</Text>
+                    <Text style={styles.musicDesc}>🎵 song by Alan Walker</Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View style={styles.card}>
+              <Image source={card1} style={styles.cardImg} />
+              <TouchableOpacity style={styles.dotIcon}>
+                <Text style={styles.dotFont}>...</Text>
+              </TouchableOpacity>
+              <View style={styles.playbox}>
+                <TouchableOpacity>
+                  <View style={styles.playboxItem}>
+                    <Text style={styles.musicName}>Faded</Text>
+                    <Text style={styles.musicDesc}>🎵 song by Alan Walker</Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View style={styles.card}>
+              <Image source={card1} style={styles.cardImg} />
+              <TouchableOpacity style={styles.dotIcon}>
+                <Text style={styles.dotFont}>...</Text>
+              </TouchableOpacity>
+              <View style={styles.playbox}>
+                <TouchableOpacity>
+                  <View style={styles.playboxItem}>
+                    <Text style={styles.musicName}>Faded</Text>
+                    <Text style={styles.musicDesc}>🎵 song by Alan Walker</Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View style={styles.card}>
+              <Image source={card1} style={styles.cardImg} />
+              <TouchableOpacity style={styles.dotIcon}>
+                <Text style={styles.dotFont}>...</Text>
+              </TouchableOpacity>
+              <View style={styles.playbox}>
+                <TouchableOpacity>
+                  <View style={styles.playboxItem}>
+                    <Text style={styles.musicName}>Faded</Text>
+                    <Text style={styles.musicDesc}>🎵 song by Alan Walker</Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View style={styles.card}>
+              <Image source={card1} style={styles.cardImg} />
+              <TouchableOpacity style={styles.dotIcon}>
+                <Text style={styles.dotFont}>...</Text>
+              </TouchableOpacity>
+              <View style={styles.playbox}>
+                <TouchableOpacity>
+                  <View style={styles.playboxItem}>
+                    <Text style={styles.musicName}>Faded</Text>
+                    <Text style={styles.musicDesc}>🎵 song by Alan Walker</Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View style={styles.card}>
+              <Image source={card1} style={styles.cardImg} />
+              <TouchableOpacity style={styles.dotIcon}>
+                <Text style={styles.dotFont}>...</Text>
+              </TouchableOpacity>
+              <View style={styles.playbox}>
+                <TouchableOpacity>
+                  <View style={styles.playboxItem}>
+                    <Text style={styles.musicName}>Faded</Text>
+                    <Text style={styles.musicDesc}>🎵 song by Alan Walker</Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View style={styles.card}>
+              <Image source={card1} style={styles.cardImg} />
+              <TouchableOpacity style={styles.dotIcon}>
+                <Text style={styles.dotFont}>...</Text>
+              </TouchableOpacity>
+              <View style={styles.playbox}>
+                <TouchableOpacity>
+                  <View style={styles.playboxItem}>
+                    <Text style={styles.musicName}>Faded</Text>
+                    <Text style={styles.musicDesc}>🎵 song by Alan Walker</Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            </View>
+
+
+          </View>
+        </ScrollView>
+        {/* Trending cards end */}
+      </View>
+      {/* Sub container end */}
+
+      <Footer navigation={navigation} />
+    </View>
+  );
+}
+
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor : 'rgba(0, 0, 0, 0.65)',
-        width: '100%' ,
-        alignContent: 'center',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    subCont:{
-        height: '100%',
-        width : '100%',
-        backgroundColor: 'rgba(000,000,000,0.2)',
-        padding: 15,
-        gap: 20,
-    },
-    searchHeader:{
-        height:35,
-        width:'50%',
-        marginTop: 45,
-        flexDirection: 'row',
-        gap: 20, 
-    },
-    imgIcons:{
-        height: 15,
-        width: 15,
-        tintColor : '#fff',
-    },
-    headerItems:{
-        flexDirection: 'row',
-        padding: 9,
-        aligncontent: 'center',
-        alignItems : 'center',
-        backgroundColor : 'rgba(255,255,255,0.2)',
-        borderRadius : 10,
-    },
-    searchtext:{
-        fontSize:16,
-        color : '#ffff',
-        opacity: 0.45,
-    },
-    mainHeading:{
-        fontSize:22,
-        color: '#c4c4c4',
-        fontWeight: '700'
-    },
-    card:{
-         width:'70%',
-         borderRadius:45,
-         height: 185,
-         resizeMode:'contain',
-    },
-    cardImg:{
-        width:'100%',
-        height: 185,
-        borderRadius:45,
-        resizeMode:'contain'
-    },
-     dotIcon: {
-       position: 'absolute',
-       right:22,
-    },
-   
-    dotFont: {
-        color: '#fff',
-        fontSize:29,
-        fontWeight:'600',
-    },
-    playbox:{
-        position: 'absolute',
-        zIndex:999,
-        bottom:0,
-        padding:15,
-        width:'100%',
-        
-    },
-    musicName:{
-        color:'#FFFEFE',
-        fontWeight : '900',
-        fontSize:16,
-    },
-    musicDesc:{
-        color:'#FFFEFE',
-        fontWeight : '100',
-        bottom:1,
-        fontSize:12,
-       
-    },
-    playboxItem:{
-      //  backgroundColor: 'rgba(61,41,148,0.5)'
-      backgroundColor: '#2E1F79',
-      height:55,
-      width:'100%',
-      padding:10,
-      borderRadius:15,
-    }
+  container: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.65)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  subCont: {
+    flex: 1,
+    width: '100%',
+    padding: 15,
+  },
+  searchHeader: {
+    flexDirection: 'row',
+    marginTop: 45,
+    marginBottom: 15,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  imgIcons: {
+    height: 15,
+    width: 15,
+    tintColor: '#fff',
+  },
+  headerItems: {
+    flexDirection: 'row',
+    padding:11,
+    alignItems: 'center',
+    backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    borderRadius: 10,
+  },
+  searchtext: {
+    fontSize: 16,
+    color: '#ffff',
+    opacity: 0.45,
     
-})
+  },
+  mainHeading: {
+    fontSize: 22,
+    color: '#c4c4c4',
+    fontWeight: '700',
+  },
+  cardContainer: {
+    flexDirection: 'row',
+    gap: 8,
+  },
+  card: {
+    width: 250,
+    borderRadius: 45,
+    height: 185,
+    resizeMode: 'contain',
+  },
+  cardImg: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 45,
+    resizeMode: 'cover',
+  },
+  dotIcon: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+  },
+  dotFont: {
+    color: '#fff',
+    fontSize: 29,
+    fontWeight: '600',
+  },
+  playbox: {
+    position: 'absolute',
+    zIndex: 999,
+    bottom: 0,
+    padding: 15,
+    width: '100%',
+  },
+  musicName: {
+    color: '#FFFEFE',
+    fontWeight: '900',
+    fontSize: 16,
+  },
+  musicDesc: {
+    color: '#FFFEFE',
+    fontWeight: '100',
+    fontSize: 12,
+  },
+  playboxItem: {
+    backgroundColor: '#2E1F79',
+    height: 55,
+    width: '100%',
+    padding: 10,
+    borderRadius: 15,
+  },
+});
